40c5c755ca5fd4a7150f1d445a77e83596f70ed2
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-06-21 04:43:59 +0200

    feat: add argc and argv

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s <input.mp4> <output.mp4> <width> <height>\n", argv[0]);
+        return 1;
+    }
+
+    const char *input_file = argv[1];
+    const char *output_file = argv[2];
+    int width = atoi(argv[3]);
+    int height = atoi(argv[4]);
+
+    if (width % 2 != 0 || height % 2 != 0) fprintf(stderr, "Error: width and height must be odd\n");
+
     return 0;
 }
